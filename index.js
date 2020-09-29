a70a6583de9a8d6d7b971ad36b718c7a19e78b64
Vadim Storozhuk <vadozy@yahoo.com>
2020-09-29 00:45:31 -0400

    End of Ch 02

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const express = require('express');
 
 const users = require('./fixtures/users');
 const emails = require('./fixtures/emails');
 
-let server = http.createServer((req, res) => {
+const app = express();
+
+app.use((req, res) => {
   let route = `${req.method} ${req.url}`;
 
   if (route === 'GET /users') {
-    res.end(JSON.stringify(users));
+    //res.end(JSON.stringify(users));
+    res.send(users);
   } else if (route === 'GET /emails') {
-    res.end(JSON.stringify(emails));
+    //res.end(JSON.stringify(emails));
+    res.send(emails);
   } else {
     res.end('You asked for ' + route);
   }
@@ -17,4 +22,5 @@ let server = http.createServer((req, res) => {
   res.end(`You asked for ${route}`);
 });
 
+const server = http.createServer(app);
 server.listen(3000);
