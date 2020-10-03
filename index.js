3eeaae553c456f8bf490bf56071325b5aa3534ed
Vadim Storozhuk <vadozy@yahoo.com>
2020-10-03 13:56:50 -0400

    End of Ch 05

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const logger = require('./lib/logger');
+
 const express = require('express');
 
 const usersRouter = require('./routes/users');
 const emailsRouter = require('./routes/emails');
 
 const app = express();
+
+app.use(logger);
+
 app.use('/users', usersRouter);
 app.use('/emails', emailsRouter);
 
--- routes/emails.js
+++ routes/emails.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const emails = require('../fixtures/emails');
-const readBody = require('../lib/read-body');
 const generateId = require('../lib/generate-id');
+const jsonBodyParser = require('../lib/json-body-parser');
 
 const emailsRouter = express.Router();
 
@@ -11,30 +11,30 @@ const getEmailsRoute = (req, res) => {
 
 const createEmailRoute = async (req, res) => {
   console.log('Creating email...');
-  const body = await readBody(req);
-  let newEmail = { ...JSON.parse(body), id: generateId() };
+  const body = req.body;
+  let newEmail = { ...body, id: generateId() };
   emails.push(newEmail);
   res.status(201);
   res.send(newEmail);
 };
 
 const updateEmailRoute = async (req, res) => {
-  let body = await readBody(req);
-  let email = emails.find(email => email.id === req.params.id);
-  Object.assign(email, JSON.parse(body));
+  const body = req.body;
+  const email = emails.find(email => email.id === req.params.id);
+  Object.assign(email, body);
   res.status(200);
   res.send(email);
 };
 
 let deleteEmailRoute = (req, res) => {
-  let index = emails.findIndex(email => email.id === req.params.id);
+  const index = emails.findIndex(email => email.id === req.params.id);
   emails.splice(index, 1);
   res.sendStatus(204);
 };
 
 emailsRouter.get('/', getEmailsRoute);
-emailsRouter.post('/', createEmailRoute);
-emailsRouter.patch('/:id', updateEmailRoute);
+emailsRouter.post('/', jsonBodyParser, createEmailRoute);
+emailsRouter.patch('/:id', jsonBodyParser, updateEmailRoute);
 emailsRouter.delete('/:id', deleteEmailRoute);
 
 module.exports = emailsRouter;
